252f7d19dc97ffefdfa2a2622e9c24013b85068f
Loren Cerri <lorencerri@gmail.com>
2021-02-01 13:50:46 -0800

    add more dependencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import "./App.css";
+import './App.css';
 
-function App() {
-    return <div className="App"></div>;
-}
+const App = () => {
+	console.log('Running...');
+
+	return <></>;
+};
 
 export default App;
